9699f54570bce89c1b70a08a24ea59d8319ff1ae
mo6709 <mo6709@gmail.com>
2017-02-02 12:40:18 -0500

    add slug feature to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
 	has_many :user_deals
 	has_many :deals, through: :user_deals
 	validates_presence_of :username, :password, :email 
+
+	def slug
+		self.username.downcase.gsub(" ","-")
+	end
+
+	def self.find_by_slug(slug)
+		self.all.find{|item| item.slug == slug}
+	end
 end
