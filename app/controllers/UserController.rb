566247c4e3f54c028fabf16f5ef6c41d540ba994
mo6709 <mo6709@gmail.com>
2017-02-02 12:14:42 -0500

    add the login get and post action

--- app/controllers/UserController.rb
+++ app/controllers/UserController.rb
@@ -9,19 +9,31 @@ class UserController < ApplicationController
   	@user = User.new(username: params[:username], password: params[:password], email: params[:email])
   	if @user.save 
   		session[:user_id] = @user.id
-  		binding.pry
+  		# flash[:message] = 
   		redirect to '/deals'
   	else
+  		# flash[:message] =
   		redirect to '/signup'
   	end
   end
 
-  get '/login' do
-  	erb :'regestrations/login'
+  get '/login' do 	
+  	if logged_in?
+      redirect to '/deals'  
+  	else
+      erb :'regestrations/login'		
+  	end
   end
 
   post '/login' do
-  	"hello"
+  	@user = User.find_by(username: params[:username])
+  	if @user && @user.authenticate(params[:password])
+  		# flash[:message]
+  		redirect to '/deals'
+  	else
+        # flash[:message]
+        redirect to '/login'
+  	end 
   end
 
 end
