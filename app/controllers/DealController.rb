1728d9a30cb8e0a85884084e267ea1657ef519a6
mo6709 <mo6709@gmail.com>
2017-02-03 13:45:30 -0500

    add a new deals post action + edit the new_deal file

--- app/controllers/DealController.rb
+++ app/controllers/DealController.rb
@@ -17,7 +17,16 @@ class DealController < ApplicationController
 	end
 
 	post '/deals' do 
-		
+		binding.pry
+		@deal = Deal.create(
+			                 importance_rate: params[:deal][:importance_rate],
+		                     title: params[:deal][:title],
+		                     starting_date: params[:deal][:starting_date],
+		                     experation_date: params[:deal][:experation_date],
+		                     link: params[:deal][:link],
+		                     content: params[:deal][:content],
+		                     category_id: params[:deal][:category_id])
+		@deal.category= Category.create(params[:category]) unless params[:category][:name].empty?
 	end
 
 end
