4fb556c1a6a95e44f18716ccf1054811108e9a19
mo6709 <mo6709@gmail.com>
2017-02-06 14:37:39 -0500

    edit categories controller

--- app/controllers/CategoryController.rb
+++ app/controllers/CategoryController.rb
@@ -1,2 +1,10 @@
 class CategoryController < ApplicationController
+	get '/categories' do
+		erb :'categories/index'
+	end 
+
+	get '/categories/:slug' do
+		erb :'categories/show_category'
+	end
+
 end
