6f30f74dc06040420af53102c30ff21567db91ed
mo6709 <mo6709@gmail.com>
2017-02-06 14:32:36 -0500

    add the slug metheds to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ActiveRecord::Base
 	has_many :deals
+
+	def slug
+		self.name.downcase.gsub(" ", "-")
+	end
+
+	def self.find_by_slug(slug)
+		self.all.find{|item| item.slug == slug}
+	end
 end
