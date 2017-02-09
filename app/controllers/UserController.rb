44e1599df36d9fb287b594371ac2501aa66acb11
mo6709 <mo6709@gmail.com>
2017-02-09 15:48:02 -0500

    add flash_messages to the entire controller

--- app/controllers/UserController.rb
+++ app/controllers/UserController.rb
@@ -13,10 +13,10 @@ class UserController < ApplicationController
   	@user = User.new(username: params[:username], password: params[:password], email: params[:email])
   	if @user.save 
   		session[:user_id] = @user.id
-  		# flash[:message] = 
+  		flash[:message] = "User were created successfully" 
   		redirect to '/deals'
   	else
-  		# flash[:message] =
+  		flash[:message] = "User can't be created, please fill up all the fields"
   		redirect to '/signup'
   	end
   end
@@ -34,11 +34,11 @@ class UserController < ApplicationController
   post '/login' do
   	@user = User.find_by(username: params[:username])
   	if @user && @user.authenticate(params[:password])
-  		# flash[:message]
+  		flash[:message] = "You are logged in succes sfully"
       session[:user_id] = @user.id
   		redirect to '/deals'
   	else
-        # flash[:message]
+        flash[:message] = "Please try again"
         redirect to '/login'
   	end 
   end
@@ -60,14 +60,14 @@ class UserController < ApplicationController
       user.email = params[:email] unless params[:email].empty?
       change_password
       if user.save
-        # flash[:message]
+        flash[:message] = "You have successfully updated your info"
       else
-        # flash[:message]
-        binding.pry
+        flash[:message] = "Sorry, something went wrong. Please go back to update your info"
       end
 
       redirect '/deals'
     else
+      flash[:message] = "Please login"
       redirect to '/login'
     end
   end
@@ -75,8 +75,10 @@ class UserController < ApplicationController
   get '/logout' do
     if logged_in?
         session.clear
+        flash[:message] = "You have successfully logged out"
         redirect to '/'
     else
+        flash[:message] = "Please login"
         redirect to '/'
     end
   end
